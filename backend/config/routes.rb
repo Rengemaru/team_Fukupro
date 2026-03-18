abfcfec14ffd7c3307d092134aa739f8ae3c83fc
rengemaru <t5424035@fukuyama-u.ac.jp>
2026-03-18 14:28:47 +0900

    feat: POST /api/villageエンドポイントを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     post "weather", to: "weather#create"
     resources :sessions, param: :token, only: [ :create, :show, :update ]
     resource  :map, only: [ :show ]
+    resources :villages, only: [ :create ]
   end
 end
